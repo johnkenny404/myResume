c1b20ef36dee5a75f2453cd2732378cb01de754e
johnkenny404 <johnkenny404@gmail.com>
2021-08-19 19:06:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const displayName = function (fulname){
+    const printName = `my name is ${fulname}`;
+    return printName;
+}
+
+console.log(displayName('jack jade'));
+
+document.write(displayName('Salihu Musa John'));
